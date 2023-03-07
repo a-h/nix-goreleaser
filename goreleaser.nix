9e1016cbb0babc1686646c1e347a82a204f93f9a
Adrian Hesketh <adrianhesketh@hushmail.com>
2023-03-07 09:08:07 +0000

    v1.16.0

--- goreleaser.nix
+++ goreleaser.nix
@@ -8,10 +8,10 @@ let
 
   suffixToHash = {
     # Use `print-hashes.sh` to generate the list below
-    Linux_x86_64 = "1a9a70y5f0phpymcvdk2i9mb42i01rmfflk7qcjhgn2nax2w8c0p";
-    Linux_arm64 = "0s4ipd8743vfbxlj78vd2hd3cvk68sjskhny3qp4bvmm5vx0mgs1";
-    Darwin_x86_64 = "0p9kbl3556mqrdjqbd5sasvmy263npskrp7kizri7fapz8jv3x2l";
-    Darwin_arm64 = "1hzx6ng8n85z7b08v20mhri2asqy96lb96l2sg05h5whavvsg8w7";
+    Linux_x86_64 = "19n5ywhg8gdav611w0iil1drnr4l1ba7akb8andi9fk54h8r70a9";
+    Linux_arm64 = "0j28cddj5vvw6ni0hpgzazsn5qmg4pmmm23vqlrw535xvixi40jw";
+    Darwin_x86_64="0mfsv7r2gm8zichc3rgfx45ivwfcvw6yjnl19rwmh0damir5c6yr";
+    Darwin_arm64 = "1z6na7q4ws1mxbyiimp8vagb0wvfl9pqd4pvcisz62hw7wkza2fr";
   };
 
   mapPlatformToArchitecture = platform: {
@@ -19,7 +19,7 @@ let
     "aarch64" = "arm64";
   }.${platform.parsed.cpu.name} or (throw "Unsupported CPU ${platform.parsed.cpu.name}");
 
-  version = "v1.15.2";
+  version = "v1.16.0";
 
   mapPlatformToSuffix = platform: "${mapPlatformToName platform}_${mapPlatformToArchitecture platform}";
 
--- print-hashes.sh
+++ print-hashes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.15.2/goreleaser_Linux_x86_64.tar.gz
-nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.15.2/goreleaser_Linux_arm64.tar.gz
+nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.16.0/goreleaser_Linux_x86_64.tar.gz
+nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.16.0/goreleaser_Linux_arm64.tar.gz
 
-nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.15.2/goreleaser_Darwin_x86_64.tar.gz
-nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.15.2/goreleaser_Darwin_arm64.tar.gz
+nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.16.0/goreleaser_Darwin_x86_64.tar.gz
+nix-prefetch-url https://github.com/goreleaser/goreleaser/releases/download/v1.16.0/goreleaser_Darwin_arm64.tar.gz
